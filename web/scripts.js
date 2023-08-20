8dd128c29c08c1e691925933e72b723ee2959e6c
mannyagre <manuel.aguirre@potenciatujuego.mx>
2023-08-20 00:59:15 -0700

    primera de highcharts

--- web/scripts.js
+++ web/scripts.js
@@ -1,3 +1,5 @@
+let agentDataHistory = {};
+
 async function connectWebSocket() {
     await eel.start_websocket()();
     await updateMessages();
@@ -5,71 +7,85 @@ async function connectWebSocket() {
 
 async function updateMessages() {
     let messages = await eel.get_received_messages()();
-    
-    // Limpiar gráficas e información anteriores
     let chartContainer = document.getElementById('chart-container');
-    chartContainer.innerHTML = '';
 
-    // Crear una sección para cada agente
-    messages.forEach((agentData, index) => {
+    messages.forEach((agentData) => {
+        let hostname = agentData['Info general']['Hostname'];
         let ip = agentData['Info general']['IP'];
 
-        // Crear un contenedor para el agente
-        let agentContainer = document.createElement('div');
-        agentContainer.className = 'agent-container';
-        chartContainer.appendChild(agentContainer);
+        // Inicializar el historial de datos para el agente si es necesario
+        if (!agentDataHistory[hostname]) {
+            agentDataHistory[hostname] = {
+                'CPU Temp': [],
+                'Motherboard Temp': [],
+                'CPU Usage': [],
+                'RAM Used': [],
+                'RAM Available': [],
+                'Storage Used': []
+            };
+        }
 
-        // Añadir información textual
-        let infoText = `
-            <h3>Agente ${ip}</h3>
-            <p>Hostname: ${agentData['Info general']['Hostname']}</p>
-            <p>MAC Address: ${agentData['Info general']['MAC address']}</p>
-            <p>Temperatura CPU: ${agentData['Procesador']['Temperatura']}</p>
-        `;
-        agentContainer.innerHTML = infoText;
-
-        // Crear un elemento canvas para la gráfica
-        let canvas = document.createElement('canvas');
-        canvas.id = `chart-${ip}`;
-        agentContainer.appendChild(canvas);
-
-        // Datos para la gráfica
-        let labels = ['Uso de CPU', 'RAM Usada', 'RAM Disponible', 'Almacenamiento Usado'];
-        let data = [
-            parseFloat(agentData['Procesador']['Uso de CPU']),
-            parseFloat(agentData['Memoria RAM']['Usada']),
-            parseFloat(agentData['Memoria RAM']['Disponible']),
-            parseFloat(agentData['Almacenamiento']['Usado'])
-        ];
-
-        // Crear la gráfica
-        new Chart(canvas, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: `Agente ${ip}`,
-                    data: data,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)'
-                    ],
-                    borderWidth: 1
-                }]
+        // Actualizar el historial de datos
+        for (let metric in agentDataHistory[hostname]) {
+            let value = 0;
+            switch (metric) {
+                case 'CPU Temp': value = agentData['Procesador']['Temperatura']; break;
+                case 'Motherboard Temp': value = agentData['Tarjeta Madre']['Temperatura']; break;
+                case 'CPU Usage': value = agentData['Procesador']['Uso de CPU']; break;
+                case 'RAM Used': value = agentData['Memoria RAM']['Usada']; break;
+                case 'RAM Available': value = agentData['Memoria RAM']['Disponible']; break;
+                case 'Storage Used': value = agentData['Almacenamiento']['Usado']; break;
             }
-        });
+            agentDataHistory[hostname][metric].push(parseFloat(value));
+        }
+
+        // Crear o actualizar el contenedor del agente
+        let agentContainer = document.getElementById(`agent-${hostname}`);
+        if (!agentContainer) {
+            agentContainer = document.createElement('div');
+            agentContainer.id = `agent-${hostname}`;
+            agentContainer.className = 'agent-container';
+            chartContainer.appendChild(agentContainer);
+        }
+
+        // Actualizar la información textual
+        let infoContainer = document.getElementById(`info-${hostname}`);
+        if (!infoContainer) {
+            infoContainer = document.createElement('div');
+            infoContainer.id = `info-${hostname}`;
+            agentContainer.appendChild(infoContainer);
+        }
+        infoContainer.innerHTML = `
+            <h3>Agente ${hostname}</h3>
+            <p>IP: ${ip}</p>
+            <p>MAC Address: ${agentData['Info general']['MAC address']}</p>
+            <p>CPU: ${agentData['Procesador']['Nombre']}</p>
+            <p>Motherboard: ${agentData['Tarjeta Madre']['Nombre']}</p>
+        `;
+
+        // Crear o actualizar las gráficas
+        for (let metric in agentDataHistory[hostname]) {
+            let chartId = `chart-${hostname}-${metric.replace(/ /g, '-')}`;
+            let chartDiv = document.getElementById(chartId);
+            if (!chartDiv) {
+                chartDiv = document.createElement('div');
+                chartDiv.id = chartId;
+                agentContainer.appendChild(chartDiv);
+            }
+
+            Highcharts.chart(chartId, {
+                title: {
+                    text: metric
+                },
+                series: [{
+                    name: metric,
+                    data: agentDataHistory[hostname][metric]
+                }]
+            });
+        }
     });
 }
 
-// Actualizar cada segundo
-setInterval(async () => {
-    await updateMessages();
-}, 1000);
+// Iniciar la conexión y actualizar cada segundo
+connectWebSocket();
+setInterval(updateMessages, 1000);
